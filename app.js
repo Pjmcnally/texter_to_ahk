37a499550d89d8772170b54c35bd42d3eda577e7
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-09-17 23:04:08 -0500

    finish minimum viable product

--- app.js
+++ app.js
@@ -21,16 +21,55 @@ function loaded(evt) {
     allItems = Array.prototype.concat.apply(allItems, doc.getElementsByClassName("row0"));
     allItems = Array.prototype.concat.apply(allItems, doc.getElementsByClassName("row1"));
 
+    // Add header to set standard settings and change hotstring ending keys
+    var header = `; Auto-Execute Section
+; Please do not edit this section unless you know what you are doing.
+; ==============================================================================
+#SingleInstance, Force              ; Automatically replaces old script with new if the same script file is rune twice
+#NoEnv                              ; Avoids checking empty variables to see if they are environment variables (recommended for all new scripts).
+#Warn                               ; Enable warnings to assist with detecting common errors. (More explicit)
+#Hotstring EndChars \`n \`t           ; Limits hotstring ending characters to {Enter}{Tab}{Space}
+SendMode Input                      ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir, %A_ScriptDir%\\..     ; Ensures a consistent starting directory. Relative path to AHK folder from core.ahk.
+SetTitleMatchMode, 2                ; 2: A window's title can contain WinTitle anywhere inside it to be a match.
+
+Return
+; ================================= ; End of Auto-Execute Section
+
+; Personal Hotstring Section.
+; Add your hotstrings here. For more information see: https://www.autohotkey.com/docs/Hotstrings.htm
+; ==============================================================================`
+
+    var output = [];
+    output.push(header)
     allItems.forEach(function(element) {
-        // TODO: Change send to sendInput (check loop section)
-        // TODO: Change hotstring ending keys
-        // TODO: Remove script tags
-        // TODO: Convert %ds --> %A_MM%/%A_DD%/%A_YYYY%
-        // TODO: Convert %td --> This might be time but I am not sure what the "d" is
-        // TODO: Convert %c --> ^v
-        // TODO: Convert %| --> Return cursor to here after replacement
-        console.log("Hotstring: ", element.childNodes[0].innerText, "Text: ", element.childNodes[1].innerText);
+        var hotString = element.childNodes[0].innerText.trim()
+        var resultText = element.childNodes[1].innerText.trim()
+
+        // Convert %| --> Return cursor to here after replacement
+        if (resultText.includes("%|")) {
+            var tmp = resultText.split("%|")
+            resultText = tmp.join("")
+            resultText += `{left ${tmp[1].length}}`
+        }
+
+        if (resultText.includes("%ds")) {
+            var tempString = `:co:${ hotString }::\n\tSendInput ${ resultText }\nreturn`
+        } else {
+            var tempString = `:co:${ hotString }::${ resultText }`
+        }
+
+        tempString = tempString.replace("::scr::", "")  // Remove script tags
+        tempString = tempString.replace("%c", "^v")  // Convert %c --> ^v
+        tempString = tempString.replace("!", "{!}")  // Fix ! issue in raw ahk string
+        tempString = tempString.replace("%ds", "%A_MM%/%A_DD%/%A_YYYY%")  // Convert %ds to ahk date
+        tempString = tempString.replace("%td", "%A_MM%/%A_DD%/%A_YYYY%")  // Convert %td to ahk date
+
+        output.push(tempString)
     });
+
+    document.getElementById("ui").hidden = "true"
+    document.getElementById("resultsBox").textContent = output.join('\n')
 }
 
 /* For reference:
